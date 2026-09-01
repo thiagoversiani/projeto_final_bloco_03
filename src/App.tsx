dd0c1bb334b60ec423e7e41c37c20a9353655e18
thiagoversiani <thiagoversi@gmail.com>
2026-09-01 11:06:47 -0300

    :sparkles: Criação de Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
-
 import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
- 
+
 function App() {
   return (
-<BrowserRouter>
-<div className="min-h-screen flex flex-col justify-between bg-slate-100">
-<Navbar />
-<div className="flex-1 flex flex-col">
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/home" element={<Home />} />
-</Routes>
-</div>
-<Footer />
-</div>
-</BrowserRouter>
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col justify-between bg-slate-100">
+        <Navbar />
+        <div className="flex-1 flex flex-col">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
- 
+
 export default App;
